1e1e5593caf1803de2c315340c485b0dca410081
Jarbas Filho <jarbasfilho29@gmail.com>
2022-07-01 22:08:33 -0300

    menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const accordionItemHeaders = document.querySelectorAll(
 );
 const accordionItemBody = document.querySelectorAll('.accordion-item-body');
 
+
+
 accordionItemHeaders.forEach((accordionItem) => {
   accordionItem.addEventListener("click", () => {
     accordionItem.classList.toggle('active')
@@ -14,3 +16,34 @@ accordionItemHeaders.forEach((accordionItem) => {
     }
   });
 });
+
+const header = document.getElementById('js-header')
+
+
+
+
+function fixedMenu() {
+  if(window.pageYOffset > 80) {
+    header.classList.add('fixed-menu');
+  } else{
+    header.classList.remove('fixed-menu');
+  }
+}
+
+document.addEventListener('scroll', fixedMenu);
+
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const btnClose = document.getElementById('js-close-menu');
+const overlayMenu = document.querySelector('.js-overlay');
+
+function handleClick(event) {
+  event.preventDefault();
+  document.documentElement.classList.toggle('menu-opened');
+  
+}
+
+
+
+btnMenu.addEventListener('click', handleClick)
+btnClose.addEventListener('click', handleClick);
+overlayMenu.addEventListener('click', handleClick)
